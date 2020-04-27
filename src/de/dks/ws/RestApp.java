db6fa4af5507a5d46e13e869a041ddb14e7298f4
anish-fullstack <abhatta266@gmail.com>
2020-04-27 21:45:04 +0530

    first commit

---
+++ src/de/dks/ws/RestApp.java
@@ -0,0 +1,9 @@
+package de.dks.ws;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/resources")
+public class RestApp extends Application {
+
+}
---
+++ src/de/dks/ws/RestService.java
@@ -0,0 +1,34 @@
+package de.dks.ws;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+
+@Path("/MyRestService")
+public class RestService {
+
+	// http://localhost:8080/RestExample/resources/MyRestService/sayHello
+	@GET
+	@Path("/sayHello")
+	public String getHelloMsg() {
+		return "Hello World";
+	}
+
+	@GET
+	@Path("/echo")
+	public Response getEchoMsg(@QueryParam("message") String msg) {
+		return Response.ok("Your message was: " + msg).build();
+	}
+
+	@GET
+	@Path("/object")
+	@Produces({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
+	public SimpleObject getObject() {
+		return new SimpleObject(1, "Test");
+	}
+
+}
